c0baedf0f7fd64e5fe4ff42265fcb2c5d7f73db8
Mats Rynge <rynge@isi.edu>
2020-08-13 12:08:57 -0700

    Fixing up environment

--- .singularity.d/env/90-osg.sh
+++ .singularity.d/env/90-osg.sh
@@ -3,3 +3,10 @@
 PS1="Singularity $SINGULARITY_NAME:\\w> "
 export PS1
 
+LD_LIBRARY_PATH=/usr/local/cuda/lib64:/.singularity.d/libs
+export LD_LIBRARY_PATH
+
+PATH=/usr/local/cuda/bin:/usr/local/bin:/usr/bin:/bin
+export PATH
+
+
